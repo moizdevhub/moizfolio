ceeaff99da1188d7adb097720ccbb3cb7e714d71
abdulmoiz001CD <abdulmoizs2005@gmail.com>
2025-08-02 20:39:06 +0500

    Add About section layout to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 // This code belongs in a JSX/TSX file, like app/page.tsx or a component
 import React from 'react';
+import AboutMe from '@/components/sections/About';
+
+
 
 const Home = () => {
   return (
-    // Correctly applying the background with a string path in a Tailwind class
-    // You should remove the `import grid_image from ...` from your .js file if you use this method
+   
     <div className="bg-[url('/grid_bg.png')] bg-cover bg-center h-96 w-full">
-      <h1 className="text-primary text-4xl p-10">Welcome to My Portfolio</h1>
+      <AboutMe />
     </div>
   );
 };
+
 
 export default Home;
